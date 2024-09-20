7bb65c4ac8860d2ec9d065bfe5aa7b819e0268d4
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-20 13:38:19 -0600

    about component

--- sura/src/App.jsx
+++ sura/src/App.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Dishes from "./components/Dishes";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <div>
         <Navbar />
+
         <main>
           <div id="home">
             <Home />
           </div>
+
           <div id="dishes">
             <Dishes />
+          </div>
+
+          <div id="about">
+            <About />
           </div>
         </main>
       </div>
