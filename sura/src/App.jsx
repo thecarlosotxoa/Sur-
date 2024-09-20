e5da27eac4ba711658b863f923ba004c0597ec93
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-19 22:52:37 -0600

    added home and dishes

--- sura/src/App.jsx
+++ sura/src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Dishes from "./components/Dishes";
 
 const App = () => {
   return (
     <>
-        <div>
-          <Navbar />
-        </div>
+      <div>
+        <Navbar />
+        <main>
+          <div id="home">
+            <Home />
+          </div>
+          <div id="dishes">
+            <Dishes />
+          </div>
+        </main>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
