7727fb169ca0cf5ba507edd5f0a491f39d0352d9
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-20 13:33:51 -0600

    edits

--- sura/src/components/Dishes.jsx
+++ sura/src/components/Dishes.jsx
@@ -11,7 +11,7 @@ const Dishes = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center lg:px-32 px-5">
       <h1 className="text-4xl font-semibold text-center pt-24 pb-10">
-        Our Dishes
+        Nuestros platos
       </h1>
 
       <div className="flex flex-wrap gap-8 justify-center">
