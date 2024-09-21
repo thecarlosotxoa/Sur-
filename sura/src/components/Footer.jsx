997c550456334406d6444b8a74fe90177555b072
Carlos Ochoa <ochoa1604@gmail.com>
2024-09-20 19:40:10 -0600

    colors

--- sura/src/components/Footer.jsx
+++ sura/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white rounded-t-3xl mt-8 md:mt-0">
+    <div className="bg-suraBlue text-white rounded-t-3xl mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className="w-full md:w-1/4">
           <h1 className="font-semibold text-xl pb-4">FoodieWeb</h1>
